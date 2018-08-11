92bb6e15baf592eebe4927e7d80f8896bd9f0810
Ireneusz Kierkowski <ircykk@gmail.com>
2018-08-11 15:13:51 +0200

    Update offers listing

--- src/Rest/Offers.php
+++ src/Rest/Offers.php
@@ -19,19 +19,47 @@ class Offers extends AbstractRestResource
      * @param string $categoryId
      * @param string $phrase
      * @param string $sellerId
+     * @param string $searchMode
+     * @param int $offset
+     * @param int $limit
+     * @param string $sort
+     * @param string $include
+     * @param bool $fallback
+     * @param array $filters
      * @return ResponseInterface
      * @throws Exception
      */
-    public function listing($categoryId = '', $phrase = '', $sellerId = '')
-    {
+    public function listing(
+        $categoryId = '',
+        $phrase = '',
+        $sellerId = '',
+        $searchMode = 'REGULAR',
+        $offset = 0,
+        $limit = 60,
+        $sort = 'relevance',
+        $include = 'all',
+        $fallback = true,
+        array $filters = []
+    ) {
         if (!$categoryId && !$phrase && !$sellerId) {
             throw new InvalidArgumentException('At least one of: category id, phrase, seller id must be provided');
         }
 
-        return $this->get('/offers/listing?'.http_build_query([
-            'category.id' => $categoryId,
-            'phrase' => $phrase,
-            'seller.id' => $sellerId,
-        ]));
+        return $this->get('/offers/listing?'.http_build_query(
+            array_merge(
+                [
+                    'category.id' => $categoryId,
+                    'phrase' => $phrase,
+                    'seller.id' => $sellerId,
+                    'searchMode' => $searchMode,
+                    'offset' => $offset,
+                    'limit' => $limit,
+                    'sort' => $sort,
+                    'include' => $include,
+                    'fallback' => $fallback,
+                ],
+                $filters
+            )
+        ));
     }
 }
