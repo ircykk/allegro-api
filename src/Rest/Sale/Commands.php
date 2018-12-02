6cb1b0e6c908b4e6770bc03e215234cde8d68b54
Ireneusz Kierkowski <ireneusz@kierkowski.com>
2018-12-02 19:20:37 +0100

    Add offers price change commands

--- src/Rest/Sale/Commands.php
+++ src/Rest/Sale/Commands.php
@@ -120,4 +120,52 @@ class Commands extends AbstractRestResource
             $params
         );
     }
+
+    /**
+     * Allows price modification of multiple offers at once.
+     *
+     * @param array $params
+     * @param string $commandId
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function offerPriceChange(array $params, $commandId = '')
+    {
+        if (!$commandId) {
+            $commandId = $this->genUuid();
+        }
+
+        return $this->put('/sale/offer-price-change-commands/'.rawurldecode($commandId), $params);
+    }
+
+    /**
+     * Provides report summary for price modification command with given commandId.
+     *
+     * @param string $commandId
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function offerPriceChangeStatus($commandId)
+    {
+        return $this->get('/sale/offer-price-change-commands/'.rawurldecode($commandId));
+    }
+
+    /**
+     * Provides detailed report for price modification command with given commandId.
+     *
+     * @param string $commandId
+     * @param int $limit
+     * @param int $offset
+     * @return mixed
+     * @throws \Http\Client\Exception
+     */
+    public function offerPriceChangeTasks($commandId, $limit = 100, $offset = 0)
+    {
+        return $this->get('/sale/offer-price-change-commands/'.rawurldecode($commandId).'/tasks?'
+            .http_build_query([
+                'limit' => $limit,
+                'offset' => $offset,
+            ])
+        );
+    }
 }
